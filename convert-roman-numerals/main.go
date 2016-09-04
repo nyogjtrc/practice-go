49edf721a3b1d180efa52b8cc1809d26035627cf
nyo <billy31210@gmail.com>
2016-09-05 02:00:37 +0800

    Finish function Roman2Arabic

--- convert-roman-numerals/main.go
+++ convert-roman-numerals/main.go
@@ -6,7 +6,29 @@ func main() {
 	fmt.Println(Roman2Arabic(""))
 }
 
+var rMap = map[string]int{
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
+}
+
 // Roman2Arabic convert roman numerals to arabic
 func Roman2Arabic(s string) int {
-	return 1
+	var preChar string
+	result := 0
+
+	for _, singleRune := range s {
+		currentChar := string(singleRune)
+		if preChar != "" && rMap[preChar] < rMap[currentChar] {
+			result -= rMap[preChar] * 2
+		}
+		result += rMap[currentChar]
+		preChar = currentChar
+	}
+
+	return result
 }
