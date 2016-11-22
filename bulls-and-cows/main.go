13570448df087d5ffe97908b68544c044d25d501
nyo <billy31210@gmail.com>
2016-11-22 17:48:41 +0800

    create 4 non repeat random number

--- bulls-and-cows/main.go
+++ bulls-and-cows/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 )
+import "time"
+import "math/rand"
 
 func main() {
 	fmt.Println("Let's play bulls and cows")
@@ -28,7 +30,27 @@ func main() {
 
 // CreateAnswerNumber create a random number as answer
 func CreateAnswerNumber() string {
-	return "0000"
+	baseStrings := [10]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	var randN int
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	randN = r1.Intn(10)
+	ans1 := baseStrings[randN]
+	baseStrings[randN], baseStrings[9] = baseStrings[9], baseStrings[randN]
+
+	randN = r1.Intn(9)
+	ans2 := baseStrings[randN]
+	baseStrings[randN], baseStrings[8] = baseStrings[8], baseStrings[randN]
+
+	randN = r1.Intn(8)
+	ans3 := baseStrings[randN]
+	baseStrings[randN], baseStrings[7] = baseStrings[7], baseStrings[randN]
+
+	randN = r1.Intn(7)
+	ans4 := baseStrings[randN]
+
+	return ans1 + ans2 + ans3 + ans4
 }
 
 // CheckGuessNumber return true when answer is the same of guess number
