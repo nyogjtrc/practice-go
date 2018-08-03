f831b24c620334bdd4d0b99e6e2b43485785ef73
nyo <billy31210@gmail.com>
2018-08-03 17:13:16 +0800

    try to create RESTful API by gin

---
+++ gin/README.md
@@ -0,0 +1,20 @@
+# Practice Go - Gin
+
+
+## package manager: dep
+
+`go get -u github.com/golang/dep/cmd/dep`
+
+`dep ensure -v`
+
+## build docker image
+
+cmd already in makefile
+
+`make build`
+
+## run service
+
+cmd already in makefile
+
+`make run`
---
+++ gin/main.go
@@ -0,0 +1,25 @@
+// Package main provides ...
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/echo/:name", controllerEcho)
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func controllerEcho(c *gin.Context) {
+	name := c.Param("name")
+	c.JSON(200, gin.H{
+		"message": "hello " + name,
+	})
+}
